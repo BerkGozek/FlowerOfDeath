515a805159fb4ba209005384c28a7254c826c4f6
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-11-27 15:26:58 +0300

    ini comm susser toodd

--- FlowerOfDeath/ContentView.swift
+++ FlowerOfDeath/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        @State var onScreen = "dd";
+        @State var answer = "_____";
+        @State var ansList = ["sedat","allah","cryin","swift"];
+        
     }
 }
 

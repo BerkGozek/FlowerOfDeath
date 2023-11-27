e878281b95ca2b9410e01dd6e1210f6ba8c4db62
Berk GOZEK <144159613+BerkGozek@users.noreply.github.com>
2023-11-27 15:07:07 +0300

    Initial Commit

---
+++ FlowerOfDeath/FlowerOfDeathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerOfDeathApp.swift
+//  FlowerOfDeath
+//
+//  Created by Berk Gozek on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowerOfDeathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
